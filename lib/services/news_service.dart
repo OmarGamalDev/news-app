77a8304653e55b4690015500655a0c1f03930560
Omar Gamal <omarthefirst6206@gmail.com>
2025-08-02 00:58:40 +0300

    Add ArticleModel class and update NewsService to parse articles from API response

--- lib/services/news_service.dart
+++ lib/services/news_service.dart
@@ -1,6 +1,5 @@
-import 'dart:developer';
-
 import 'package:dio/dio.dart';
+import 'package:news_app/models/article_model.dart';
 
 class NewsService {
   final Dio dio;
@@ -8,7 +7,18 @@ class NewsService {
 
    getNews() async {
     Response response = await dio.get('https://newsapi.org/v2/top-headlines?country=us&category=general&apiKey=53607d3a0f7d40adb010465524026936#');
-    log(response.toString());
+    Map<String,dynamic>jsonData = response.data;
+    List<dynamic> articles = jsonData["articles"];
+    List<ArticleModel>articlesList =[ ];
+    for(var article in articles) {
+      ArticleModel articleModel = ArticleModel(
+        imageUrl: article["urlToImage"] ?? '',
+        title: article["title"] ?? '',
+        subTitle: article["description"] ?? '',
+      );
+      articlesList.add(articleModel);
+    }
+    print(articlesList);
   }
-  
+
 }
